9bd46fcf9c23d1505e5590538d737f753e816efb
Adithya M S <adithyashivaram@gmail.com>
2021-11-20 20:15:41 +0530

    Built 4 incremental models

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ from pyspark.streaming import StreamingContext
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 import sys
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
+import numpy as np
 
-def readMyStream(rdd,schema,spark):
+def readMyStream(rdd,schema,spark,classifierModel):
   if not rdd.isEmpty():
     df = spark.read.json(rdd)
     print('Started the Process')
@@ -23,9 +28,11 @@ def readMyStream(rdd,schema,spark):
 
     lengthdf=dataExploration(newdf)
     clean_df=preprocess(lengthdf)
-    spam_detector=model(clean_df)
-    predictions=spam_detector.transform(clean_df)
-    evaluate(predictions)
+    model(clean_df,classifierModel)
+    # X=np.array(clean_df.select('features').collect())
+    # y=np.array(clean_df.select('label').collect())
+    # predictions=classifierModel.predict(X.reshape(X.shape[0],X.shape[2]))
+    # evaluate(predictions,y.reshape(y.shape[0]))
   
 
 if __name__ == '__main__':
@@ -42,7 +49,13 @@ if __name__ == '__main__':
       [StructField("Subject",StringType(),True),
       StructField("Body",StringType(),True),
       StructField("Spam/Ham",StringType(),True)])
-  stream_data.foreachRDD(lambda rdd:readMyStream(rdd,schema,spark))
+  #classifierModel = MultinomialNB()
+  #classifierModel = SGDClassifier(alpha=0.1,n_jobs=-1,eta0=0.0,n_iter_no_change=1000)
+  #classifierModel = SGDClassifier(loss="log")
+  #classifierModel = MLPClassifier(learning_rate='adaptive',solver="sgd",activation="logistic")
+  classifierModel = PassiveAggressiveClassifier(n_jobs=-1,C=0.5,random_state=5)
+
+  stream_data.foreachRDD(lambda rdd:readMyStream(rdd,schema,spark,classifierModel))
 
   ssc.start()
   ssc.awaitTermination()
